9bf9c36eb8414a7d4af4293e0aaafc37b5382ddb
tavstal <tavstal@gmail.com>
2025-08-19 20:11:06 +0200

    Add Constants class for fuel durations and core block material

---
+++ src/main/java/io/github/tavstaldev/Constants.java
@@ -0,0 +1,47 @@
+package io.github.tavstaldev;
+
+import org.bukkit.Material;
+
+import java.time.Duration;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Constants
+{
+    public static final Map<Material, Duration> FUEL;
+
+    static {
+        Map<Material, Duration> unsortedMap = new LinkedHashMap<>();
+        unsortedMap.put(Material.COAL, Duration.ofHours(24));
+        unsortedMap.put(Material.COAL_BLOCK, Duration.ofHours(216));
+        unsortedMap.put(Material.CHARCOAL, Duration.ofHours(24));
+        unsortedMap.put(Material.LAVA_BUCKET, Duration.ofHours(120));
+        unsortedMap.put(Material.BLAZE_ROD, Duration.ofHours(48));
+        unsortedMap.put(Material.OAK_LOG, Duration.ofHours(12));
+        unsortedMap.put(Material.SPRUCE_LOG, Duration.ofHours(12));
+        unsortedMap.put(Material.BIRCH_LOG, Duration.ofHours(12));
+        unsortedMap.put(Material.JUNGLE_LOG, Duration.ofHours(12));
+        unsortedMap.put(Material.ACACIA_LOG, Duration.ofHours(12));
+        unsortedMap.put(Material.DARK_OAK_LOG, Duration.ofHours(12));
+        unsortedMap.put(Material.MANGROVE_LOG, Duration.ofHours(12));
+        unsortedMap.put(Material.CRIMSON_STEM, Duration.ofHours(12));
+        unsortedMap.put(Material.WARPED_STEM, Duration.ofHours(12));
+        // Add all other entries
+
+        FUEL = unsortedMap.entrySet().stream()
+                .sorted(Map.Entry.<Material, Duration>comparingByValue().reversed()) // Sort by duration descending
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        Map.Entry::getValue,
+                        (oldValue, newValue) -> oldValue,
+                        LinkedHashMap::new
+                ));
+    }
+
+    public static final Duration MAXIMUM_FUEL_DURATION = Duration.ofDays(7);
+
+    public static final Duration DEFAULT_FUEL_DURATION = Duration.ofDays(2);
+
+    public static final Material CORE_BLOCK_MATERIAL = Material.BARREL;
+}
