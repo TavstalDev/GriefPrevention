ebe9a25289e682337ce9aaed081391b4ace94ce3
tavstal <tavstal@gmail.com>
2025-08-19 20:11:25 +0200

    Add PlayerManager class to handle player data management

---
+++ src/main/java/io/github/tavstaldev/PlayerManager.java
@@ -0,0 +1,38 @@
+package io.github.tavstaldev;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class PlayerManager {
+    private static final Map<UUID, PlayerCache> _playerData = new HashMap<>();
+
+    /**
+     * Adds player data to the manager.
+     *
+     * @param playerId   the UUID of the player
+     * @param playerData the data associated with the player
+     */
+    public static void addPlayerData(UUID playerId, PlayerCache playerData) {
+        _playerData.put(playerId, playerData);
+    }
+
+    /**
+     * Removes player data from the manager.
+     *
+     * @param playerId the UUID of the player
+     */
+    public static void removePlayerData(UUID playerId) {
+        _playerData.remove(playerId);
+    }
+
+    /**
+     * Retrieves player data from the manager.
+     *
+     * @param playerId the UUID of the player
+     * @return the data associated with the player, or null if not found
+     */
+    public static PlayerCache getPlayerData(UUID playerId) {
+        return _playerData.get(playerId);
+    }
+}
