11cca77666b4e0b81cf5b1c29244eb6f926fa4b6
tavstal <tavstal@gmail.com>
2025-08-19 20:11:41 +0200

    Add RefreshClaimHologramTask class for refreshing claim holograms

---
+++ src/main/java/io/github/tavstaldev/RefreshClaimHologramTask.java
@@ -0,0 +1,12 @@
+package io.github.tavstaldev;
+
+public class RefreshClaimHologramTask implements Runnable
+{
+    public RefreshClaimHologramTask() {}
+
+    @Override
+    public void run()
+    {
+        // TODO: Implement the logic to refresh claim holograms
+    }
+}
