23d7fab02bf9baa5a6dbc5778817fd89e9843b91
tavstal <tavstal@gmail.com>
2025-10-02 16:47:36 +0200

    Add core block issue flag to CreateClaimResult: indicate failure due to core block conflicts

--- src/main/java/me/ryanhamshire/GriefPrevention/CreateClaimResult.java
+++ src/main/java/me/ryanhamshire/GriefPrevention/CreateClaimResult.java
@@ -25,6 +25,9 @@ public class CreateClaimResult
     //whether or not the creation succeeded (it would fail if the new claim overlapped another existing claim)
     public boolean succeeded;
 
+    // Indicates whether the claim creation failed due to a core block issue
+    public boolean coreBlockIssue;
+
     //when succeeded, this is a reference to the new claim
     //when failed, this is a reference to the pre-existing, conflicting claim
     public @Nullable Claim claim;
